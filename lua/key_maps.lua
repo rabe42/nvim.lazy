c87f7b3381b83ccaa2db56dc1e9dda2a9b31fb59
Dr Ralf Berger <r.berger@datango.com>
2024-04-07 18:14:19 +0200

    support for different terminals on key strokes.

--- lua/key_maps.lua
+++ lua/key_maps.lua
@@ -1,2 +1,5 @@
 local map = vim.api.nvim_set_keymap
-map('t', '<Esc>', '<C-\\><C-n>', {})
+map("t", "<Esc>", "<C-\\><C-n>", {})
+
+map("n", "<leader><C-t>", ":vsplit term://nu<CR>", {})
+map("n", "<leader><S-t>", ":vsplit term://powershell.exe<CR>", {})
