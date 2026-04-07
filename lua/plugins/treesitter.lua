0779035d5961bfd1f832f4b443b99ef04469a698
Dr. Ralf Berger <Dr.Ralf.Berger@gmail.com>
2026-04-07 14:56:15 +0200

    Updated treesitter config for latest changes.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-			-- ensure_installed = {"lua", "java", "javascript", "typescript", "rust"},
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    lazy = false,
+    branch = "master",
+    main = "nvim-treesitter.config",
+    opts = {
+        -- ensure_installed = {"lua", "java", "javascript", "typescript", "rust"},
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+    }
 }
